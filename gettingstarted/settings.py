0fd23cd7ef10e9603cf391a924fa52ecd07132a9
Raymond Machira <raymond.machira+git@gmail.com>
2017-06-30 15:27:36 -0500

    Remove heroku workers logic from heroku script.

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -135,4 +135,3 @@ STATICFILES_DIRS = (
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
--- heroku_deploy.sh
+++ heroku_deploy.sh
@@ -6,14 +6,9 @@ git fetch heroku
 MIGRATION_CHANGES=$(git diff HEAD heroku/master --name-only -- */migrations/* | wc -l)
 echo "$MIGRATION_CHANGES db changes."
 
-PREV_WORKERS=$(heroku ps --app $APP_NAME | grep "^worker." | wc -l | tr -d ' ')
-
 # migrations require downtime so enter maintenance mode
 if test $MIGRATION_CHANGES -gt 0; then
   heroku maintenance:on --app $APP_NAME
-
-  # Make sure workers are not running during a migration
-  heroku scale worker=0 --app $APP_NAME
 fi
 
 # deploy code changes (and implicitly restart the app and any running workers)
@@ -22,9 +17,7 @@ git push heroku $CIRCLE_SHA1:refs/heads/master
 # run database migrations if needed and restart background workers once finished
 if test $MIGRATION_CHANGES -gt 0; then
   heroku run python manage.py migrate --app $APP_NAME
-  heroku scale worker=$PREV_WORKERS --app $APP_NAME
   heroku restart --app $APP_NAME
 fi
 
-heroku run rake cache:flush --app $APP_NAME
 heroku maintenance:off --app $APP_NAME
